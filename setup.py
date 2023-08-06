a4e72d4e5736a407b553d7ec662716f5e0ff7204
Chaipat J <work.chaipat@gmail.com>
2023-08-07 03:47:54 +0700

    update python version

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+        "Programming Language :: Python :: 3.11",
     ],
     entry_points={
         'console_scripts': [
@@ -32,4 +32,4 @@ setup(
         ],
     },
 )
-)
+

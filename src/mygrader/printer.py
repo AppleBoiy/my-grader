215fd8f1e1b292206b1dfc09f9a39821fb7e6053
Chaipat J <work.chaipat@gmail.com>
2023-08-08 12:45:45 +0700

    feat(mygrader): can directly write on file in function printer.

--- src/mygrader/printer.py
+++ src/mygrader/printer.py
@@ -1,12 +1,17 @@
+from typing import List, Tuple
 
-def print_test_results(num_passed, num_failed, failed_cases):
+from .writer import write_failed_cases_to_file
+
+
+def print_test_results(num_passed: int, num_failed: int, failed_cases: List[Tuple], write_on_file: bool = False):
     """
     Print the test results and return the list of failed cases.
 
     Parameters:
-        num_passed (int): Number of test cases passed.
-        num_failed (int): Number of test cases failed.
-        failed_cases (list): List of tuples containing failed test cases.
+        :param num_passed: Number of test cases passed.
+        :param num_failed: Number of test cases failed.
+        :param failed_cases: List of tuples containing failed test cases.
+        :param write_on_file:
 
     Returns:
         list: List of dictionaries containing failed test cases.
@@ -14,33 +19,21 @@ def print_test_results(num_passed, num_failed, failed_cases):
     """
     print(f"{num_passed} test cases passed.")
 
+    failed_cases_list = []
 
     if num_failed > 0:
         print(f"{num_failed} test cases failed.")
 
         for i, failed_case in enumerate(failed_cases, start=1):
-            if i > 10:
-                break
+            if i < 10:
+                print(f"{i}. Input: {failed_case[0]}, Expected: {failed_case[1]}, Got: {failed_case[2]}")
+            failed_cases_list.append(
+                {"Input": failed_cases[i][0], "Expected": failed_case[i][1], "Got": failed_cases[i][2]}
+            )
 
-            print(f"{i}. Input: {failed_case[0]}, Expected: {failed_case[1]}, Got: {failed_case[2]}")
+        if write_on_file:
+            write_failed_cases_to_file(failed_cases_list)
+
 
     else:
         print("All test cases passed successfully.")
-
-    return [
-        {"Input": case[0], "Expected": case[1], "Got": case[2]} for case in failed_cases
-    ]
-
-
-def write_failed_cases_to_file(failed_cases, filename="failed_cases.txt"):
-    """
-    Write the list of failed cases to a file.
-
-    Parameters:
-        failed_cases (list): List of dictionaries containing failed test cases.
-        filename (str, optional): The name of the output file. Defaults to "failed_cases.txt".
-
-    """
-    with open(filename, "w") as file:
-        for i, failed_case in enumerate(failed_cases, start=1):
-            file.write(f"{i}. Input: {failed_case['Input']}, Expected: {failed_case['Expected']}, Got: {failed_case['Got']}\n")

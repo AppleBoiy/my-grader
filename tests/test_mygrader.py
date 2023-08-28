7ccf21063f5249193348f828076587f7a6dbf26d
Chaipat J. <work.chaipat@gmail.com>
2023-08-28 20:27:48 +0700

    fix(mygrader): fix import error in testing unit

--- tests/test_mygrader.py
+++ tests/test_mygrader.py
@@ -6,26 +6,23 @@ import pytest
 from mygrader import Tester
 
 
-class TestSolution:
+def calculate_sum(x: int, y: int) -> int:
+    """
+        Calculates the sum of a sequence of numbers.
 
-    @staticmethod
-    def calculate_sum(x: int, y: int) -> int:
+        Given two numbers, x and y, this function calculates the sum of the sequence
+        of numbers starting from x and ending at y, inclusive, using the formula for
+        the sum of an arithmetic series.
+
+        Parameters:
+        - x: Starting number of the sequence
+        - y: Ending number of the sequences
+
+        Returns:
+        The sum of the sequence.
         """
-            Calculates the sum of a sequence of numbers.
-
-            Given two numbers, x and y, this function calculates the sum of the sequence
-            of numbers starting from x and ending at y, inclusive, using the formula for
-            the sum of an arithmetic series.
-
-            Parameters:
-            - x: Starting number of the sequence
-            - y: Ending number of the sequences
-
-            Returns:
-            The sum of the sequence.
-            """
-        summation = int((x + y) * ((y - x + 1) / 2))
-        return summation
+    summation = int((x + y) * ((y - x + 1) / 2))
+    return summation
 
 
 class TestMyGrader:
@@ -58,7 +55,7 @@ class TestMyGrader:
         try:
             buffer = io.StringIO()
             with contextlib.redirect_stdout(buffer):
-                tester.run_test(TestSolution.calculate_sum, num_test_cases=1000)
+                tester.run_test(calculate_sum)
 
         except Exception as e:
             assert False, f"Failed to run test: {e}"
@@ -71,7 +68,7 @@ class TestMyGrader:
             buffer = io.StringIO()
             with contextlib.redirect_stderr(buffer):
                 # Run the test cases with the sphere_volume function from Solution class
-                tester.run_test(TestSolution.calculate_sum, num_test_cases=1000000)
+                tester.run_test(calculate_sum, num_test_cases=1000000)
 
             # If the code execution completes within the time limit, this point is reached
             assert False, "Failed to raise exception when runtime limit is exceeded"
